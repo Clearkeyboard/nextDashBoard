5897d513092f0f86e094b8947579fd383819572d
Clearkeyboard <byron.clearkeyboard@gmail.com>
2025-03-03 10:05:24 -0500

    added pagination

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   experimental: {
     ppr: 'incremental'
-  }
+  },
+  devIndicators: { appIsrStatus: false }
 };
 
 export default nextConfig;

8c21ac2a74ff80abaf05a33637ca02581c92e508
Clearkeyboard <byron.clearkeyboard@gmail.com>
2025-03-06 13:08:13 -0500

    removed seed routes

--- app/lib/lawyerplaceholder.ts
+++ app/lib/lawyerplaceholder.ts
@@ -18,10 +18,10 @@ const lawyers = [
 async function uploadLawyers(stringJSON:string) {
     const jsonLawyerData = JSON.parse(stringJSON);
     const insertedLawyers = await Promise.all(
-            jsonLawyerData.map((lawyer) => 
+            jsonLawyerData.map((lawyer: { FirstName: string | number | boolean | Date | Uint8Array<ArrayBufferLike> | postgres.Helper<any, any[]> | postgres.Parameter<any> | postgres.ArrayParameter<readonly any[]> | readonly postgres.SerializableParameter<never>[] | postgres.Fragment | postgres.Fragment[] | null; Email: string | number | boolean | Date | Uint8Array<ArrayBufferLike> | postgres.Helper<any, any[]> | postgres.Parameter<any> | postgres.ArrayParameter<readonly any[]> | readonly postgres.SerializableParameter<never>[] | postgres.Fragment | postgres.Fragment[] | null; Bar: string | number | boolean | Date | Uint8Array<ArrayBufferLike> | postgres.Helper<any, any[]> | postgres.Parameter<any> | postgres.ArrayParameter<readonly any[]> | readonly postgres.SerializableParameter<never>[] | postgres.Fragment | postgres.Fragment[] | null; }) => 
                 sql`
                 INSERT INTO lawyersBeta (name, personal_email, bar_number)
-                VALUES (${lawyer.FirstName},${lawyer.Email},${lawyer.Bar})
+                VALUES (${lawyer.FirstName},${lawyer.Email},${lawyer.Bar}) 
                 ON CONFLICT (bar_number) DO NOTHING;
                 `
             ),      
